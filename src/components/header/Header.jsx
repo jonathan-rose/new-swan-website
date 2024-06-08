1bcd662b90b910c3c316c9b540a2bacf844fb4fe
Jonathan Rose <jonathan_rose@outlook.com>
2024-06-08 22:25:09 +0100

    Spacing adjustments.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,13 +4,13 @@ export default function Header ({ onLinkClick }) {
     return (
         <header>
             <nav>
-                <Logo height="100px"></Logo>
+                <Logo height="110px"></Logo>
                 <a onClick={() => onLinkClick('home')}>Home</a>
                 <a onClick={() => onLinkClick('about')}>About</a>
                 <a onClick={() => onLinkClick('gallery')}>Gallery</a>
                 <a onClick={() => onLinkClick('video')}>Video</a>
                 <a onClick={() => onLinkClick('contact')}>Contact</a>
-                <img src="./images/facebook.png" height="100"/>
+                <img id="facebook" src="./images/facebook.png" height="100px"/>
             </nav>
         </header>
     )
--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,7 +1,5 @@
 export default function Logo({ height }) {
     return (
-        <div className='logo'>
-            <img className="logo" src="/images/logo/logo.png" style={{ height }}/>
-        </div>
+        <img className="logo" src="/images/logo/logo.png" style={{ height }} />
     )
 }
