df8732bd675652703efb42205a5b31dd4afb26a0
Jonathan Rose <jonathan_rose@outlook.com>
2024-06-09 04:23:37 +0100

    Links for logos.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,20 @@
 import Logo from '../logo/Logo'
 
-export default function Header ({ onLinkClick }) {
+export default function Header({ onLinkClick }) {
     return (
         <header>
             <nav>
-                <Logo height="110px"></Logo>
+                <a onClick={() => onLinkClick('home')}>
+                    <Logo height="110px"></Logo>
+                </a>
                 <a onClick={() => onLinkClick('home')}>Home</a>
                 <a onClick={() => onLinkClick('about')}>About</a>
                 <a onClick={() => onLinkClick('gallery')}>Gallery</a>
                 <a onClick={() => onLinkClick('video')}>Video</a>
                 <a onClick={() => onLinkClick('contact')}>Contact</a>
-                <img id="facebook" src="./images/facebook.png" height="100px"/>
+                <a href="https://www.facebook.com/SWAN.WSM">
+                    <img id="facebook" src="./images/facebook.png" height="100px"></img>
+                </a>
             </nav>
         </header>
     )
