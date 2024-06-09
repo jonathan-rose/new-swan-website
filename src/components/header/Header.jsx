e4796c5591ba8c86268a8e3604a6610beb275919
Jonathan Rose <jonathan_rose@outlook.com>
2024-06-09 05:07:08 +0100

    Move logo and Facebook link to banner.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,18 +3,28 @@ import Logo from '../logo/Logo'
 export default function Header({ onLinkClick }) {
     return (
         <header>
+            <div className='banner'>
+                <div className='banner-item'>
+                    <a onClick={() => onLinkClick('home')}>
+                        <Logo height="110px"></Logo>
+                    </a>
+                </div>
+                <div className='banner-item' id="banner-text">
+                    <h1>South Weston Activity Network</h1>
+                </div>
+                <div className='banner-item'>
+                    <a href="https://www.facebook.com/SWAN.WSM">
+                        <img id="facebook" src="./images/facebook.png" height="100px"></img>
+                    </a>
+                </div>
+            </div>
             <nav>
-                <a onClick={() => onLinkClick('home')}>
-                    <Logo height="110px"></Logo>
-                </a>
                 <a onClick={() => onLinkClick('home')}>Home</a>
                 <a onClick={() => onLinkClick('about')}>About</a>
                 <a onClick={() => onLinkClick('gallery')}>Gallery</a>
                 <a onClick={() => onLinkClick('video')}>Video</a>
                 <a onClick={() => onLinkClick('contact')}>Contact</a>
-                <a href="https://www.facebook.com/SWAN.WSM">
-                    <img id="facebook" src="./images/facebook.png" height="100px"></img>
-                </a>
+
             </nav>
         </header>
     )
