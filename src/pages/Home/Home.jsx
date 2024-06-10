9030a692fffedb7a1946512bc95edabfc38f407e
Jonathan Rose <jonathan_rose@outlook.com>
2024-06-10 19:11:09 +0100

    Basic home layout that is similar to previous design.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,8 +3,12 @@ import './home.css';
 export default function Home() {
     return (
         <div className="home-content">
-            <div className="home-text">Test text please ignore.</div>
-            <img className="home-image" alt="The SWAN team." src="../../images/content/park1.jpg"/>
+            <div className="home-text">
+                <h1>We are SWAN</h1>
+                <h2>We work with children, young people and families.</h2>
+                <h2>Together we can have a bigger impact.</h2>
+            </div>
+            <img className="home-image" alt="The SWAN team." src="../../images/content/park1.jpg" />
         </div>
     )
 }
