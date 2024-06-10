bd29d95c69e574f443f4f6a5c292abc6cde4d82f
Jonathan Rose <jonathan_rose@outlook.com>
2024-06-10 13:09:18 +0100

    Reduce image sizes for balance.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,7 @@ export default function Header({ onLinkClick }) {
             <div className='banner'>
                 <div className='banner-item'>
                     <a onClick={() => onLinkClick('home')}>
-                        <Logo height="100px"></Logo>
+                        <Logo></Logo>
                     </a>
                 </div>
                 <div className='banner-item'>
@@ -14,7 +14,7 @@ export default function Header({ onLinkClick }) {
                 </div>
                 <div className='banner-item'>
                     <a href="https://www.facebook.com/SWAN.WSM">
-                        <img id="facebook" src="./images/facebook.png" height="100px"></img>
+                        <img id="facebook" src="./images/facebook.png"></img>
                     </a>
                 </div>
             </div>
@@ -24,6 +24,7 @@ export default function Header({ onLinkClick }) {
                 <a onClick={() => onLinkClick('gallery')}>Gallery</a>
                 <a onClick={() => onLinkClick('video')}>Video</a>
                 <a onClick={() => onLinkClick('contact')}>Contact</a>
+                {/* <a href='https://www.facebook.com/swan.wsm/'>Facebook</a> */}
             </nav>
         </header>
     )
