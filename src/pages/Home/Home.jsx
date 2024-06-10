269e5ee836dbc7216866488285df73e6b0978949
Jonathan Rose <jonathan_rose@outlook.com>
2024-06-10 16:16:35 +0100

    Adding flex elements for home text and group image.

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import './home.css';
+
+export default function Home() {
+    return (
+        <div className="home-content">
+            <div className="home-text">Test text please ignore.</div>
+            <img className="home-image" alt="The SWAN team." src="../../images/content/park1.jpg"/>
+        </div>
+    )
+}
