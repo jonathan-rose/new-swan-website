e3fce5009cfa2cc54ae91cb20a36e0e154b7cf88
Jonathan Rose <jonathan_rose@outlook.com>
2024-06-24 15:17:46 +0100

    Move to variable for storing images.

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,21 @@
 import './about.css'
 
-export default function About () {
+const activityImages = [
+    { src: "../../images/content/park1.jpg", alt: "Test 1" },
+    { src: "../../images/content/park1.jpg", alt: "Test 2" },
+    { src: "../../images/content/park1.jpg", alt: "Test 3" },
+];
+
+export default function About() {
     return (
         <div className='about-container'>
             <section className='about-section'>
-                <div className='about-img'></div>
+                <div className='about-img'>
+                    <img
+                        className="about-img-activity"
+                        src={activityImages[0].src}
+                        alt={activityImages[0].alt} />
+                </div>
                 <div className='about-text'>
                     <p>Activity 1</p>
                 </div>
