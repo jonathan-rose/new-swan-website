efe1006590b3a995d418b9c536155fa548d0895b
Jonathan Rose <jonathan_rose@outlook.com>
2024-07-01 18:29:51 +0100

    Moved activity info elements in seperate ImgInfo component.

---
+++ src/components/ImgInfo/ImgInfo.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './ImgInfo.module.css'
+
+const ImgInfo = ({ id, imageUrl, text }) => (
+  <section className={styles.ImgInfoSection}>
+    {id % 2 === 0 ? (
+      <>
+        <div className={styles.ImgInfoTextContainer}>
+          <p>{text}</p>
+        </div>
+        <div className={styles.ImgInfoImgContainer}>
+          <img height="100%" src={imageUrl} alt="Sample Image" />
+        </div>
+      </>
+    ) : (
+      <>
+        <div className={styles.ImgInfoImgContainer}>
+          <img height="100%" src={imageUrl} alt="Sample Image" />
+        </div>
+        <div className={styles.ImgInfoTextContainer}>
+          <p>{text}</p>
+        </div>
+      </>
+    )}
+  </section>
+);
+
+export default ImgInfo;
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,14 +1,9 @@
 import './about.css'
 import React, { useState, useEffect } from 'react';
 import activityData from './activities.json';
+import ImgInfo from '../../components/ImgInfo/ImgInfo';
 
-const activityImages = [
-    { src: "../../images/content/park1.jpg", alt: "Test 1" },
-    { src: "../../images/content/park1.jpg", alt: "Test 2" },
-    { src: "../../images/content/park1.jpg", alt: "Test 3" },
-];
-
-const sections = activityData.map((item, index) => ({
+const activities = activityData.map((item, index) => ({
     id: index,
     imageUrl: item.imageUrl,
     text: item.text,
@@ -16,32 +11,16 @@ const sections = activityData.map((item, index) => ({
 
 export default function About() {
     return (
-        <div className='about-container'>
-            <div className="activities">
-                {sections.map((section) => (
-                    <section key={section.id} className="activities-section">
-                        {section.id % 2 === 0 ? (
-                            <>
-                                <div className='activities-img-container'>
-                                    <img height="100%" src={section.imageUrl} alt="About Section Image" />
-                                </div>
-                                <div className='activities-text-container'>
-                                    <p>{section.text}</p>
-                                </div>
-                            </>
-                        ) : (
-                            <>
-                                <div className='activities-text-container'>
-                                    <p>{section.text}</p>
-                                </div>
-                                <div className='activities-img-container'>
-                                    <img height="100%" src={section.imageUrl} alt="About Section Image" />
-                                </div>
-                            </>
-                        )}
-                    </section>
-                ))}
-            </div>
+      <div className="about-container">
+        <div className="activities">
+          {activities.map((section) => (
+            <ImgInfo
+              id={section.id}
+              imageUrl={section.imageUrl}
+              text={section.text}
+            />
+          ))}
         </div>
+      </div>
     );
-}
+  }
