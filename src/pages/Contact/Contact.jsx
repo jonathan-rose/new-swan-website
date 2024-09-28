0ff315c060109bcb5a9832aba1f83b0beb3470e1
Jonathan Rose <jonathan_rose@outlook.com>
2024-09-28 16:13:28 +0100

    Changed URL for Google Maps embed to give a larger scale view of Weston.

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -33,7 +33,7 @@ export default function Contact() {
                 <iframe
                     title="Healthy Living Centre Map"
                     id="map"
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2492.8532092287664!2d-2.96828892313459!3d51.332219723485466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4871f8c7dbb4decd%3A0xcdf90b4376839da1!2sFor%20All%20Healthy%20Living%20Centre!5e0!3m2!1sen!2suk!4v1709495151720!5m2!1sen!2suk"
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d8384.868256735324!2d-2.965799349809938!3d51.33253169405425!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4871f8c7dbb4decd%3A0xcdf90b4376839da1!2sFor%20All%20Healthy%20Living%20Centre!5e0!3m2!1sen!2suk!4v1727536295858!5m2!1sen!2suk"
                     allowfullscreen=""
                     loading="lazy"
                     referrerpolicy="no-referrer-when-downgrade">
