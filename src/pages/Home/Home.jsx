9a497ad43dff6716b397cdf5235721331d5dcccf
Jonathan Rose <jonathan_rose@outlook.com>
2025-04-05 18:13:16 +0100

    Test to setup GitHub actions.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <div className="home-content">
             <div className="home-text">
-                <h1>We are SWAN</h1>
+                <h1>We are SWAN!</h1>
                 <h2>We work with children, young people and families.</h2>
                 <h2>Together we can have a bigger impact.</h2>
             </div>
